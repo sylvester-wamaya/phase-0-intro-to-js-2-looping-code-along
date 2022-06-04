b91761159951149636896715f44f304dcdff1f54
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2022-06-04 16:55:03 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+function writeCards(name, event){
+    let message = [];
+for(let i = 0; i < name.length; i++){
+message.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+}
+return message;
+}
+
+function countDown(i){
+while(i > 0){
+    console.log(i);
+    i -= 1;
+}
+console.log(i);
+}
